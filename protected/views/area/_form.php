7dcadfad3061b26629cd195c7c905ac99721c5d9
Hristo Tanchev <h.tanchev@emerchantpay.com>
2014-11-03 23:22:16 +0200

    Reworked _form partial for area

--- protected/views/area/_form.php
+++ protected/views/area/_form.php
@@ -6,34 +6,39 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
+<div id="map-canvas" style="width: 50em; height: 30em; position: relative; overflow: hidden; -webkit-transform: translateZ(0px); background-color: rgb(229, 227, 223);"></div>
+
+<div class="area-form">
+    <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'area-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
 )); ?>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <div class="row">
+        <?php echo $form->labelEx($model,'area_name'); ?>
+        <?php echo $form->textField($model,'area_name',array('size'=>25,'maxlength'=>25)); ?>
+    </div>
+    <br>
+    <br>
 
-	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'area_name'); ?>
-		<?php echo $form->textField($model,'area_name',array('size'=>25,'maxlength'=>25)); ?>
-		<?php echo $form->error($model,'area_name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'geo'); ?>
-		<?php echo $form->textField($model,'geo'); ?>
-		<?php echo $form->error($model,'geo'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
+    <div class="row">
+      <?php 
+        echo CHtml::activeDropDownList($model, 'client_id', $model->getClients())
+      ?>
+    </div>
+    <br>
+    <br>
+    
+    <input type="hidden" name="points" id="points">
+    <br>
+    <br>
+    <button type="button" class="btn btn-primary" name="show" id="show" onclick="showArea()">Show Zone</button>
+    <br>
+    <br>
+    <div class="row buttons" id="create_area_button">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+    </div>
 
 <?php $this->endWidget(); ?>
 
--- protected/views/area/create.php
+++ protected/views/area/create.php
@@ -15,39 +15,6 @@ $this->menu=array(
 
 <h1>Create Area</h1>
 
-<div id="map-canvas" style="width: 50em; height: 30em; position: relative; overflow: hidden; -webkit-transform: translateZ(0px); background-color: rgb(229, 227, 223);"></div>
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>
 
-<div class="area-form">
-    <?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'area-form',
-	'enableAjaxValidation'=>false,
-)); ?>
-    <p class="note">Fields with <span class="required">*</span> are required.</p>
-
-    <div class="row">
-        <?php echo $form->labelEx($model,'area_name'); ?>
-        <?php echo $form->textField($model,'area_name',array('size'=>25,'maxlength'=>25)); ?>
-    </div>
-    <br>
-    <br>
-
-    <div class="row">
-      <?php 
-        echo CHtml::activeDropDownList($model, 'client_id', $model->getClients())
-      ?>
-    </div>
-    <br>
-    <br>
-    
-    <input type="hidden" name="points" id="points">
-    <br>
-    <br>
-    <button type="button" class="btn btn-primary" name="show" id="show" onclick="showArea()">Show Zone</button>
-    <br>
-    <br>
-    <div class="row buttons" id="create_area_button">
-        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-    </div>
-
-    <?php $this->endWidget(); ?>
 </div>
