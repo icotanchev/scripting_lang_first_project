859fc374492a42767400d0e47ae735ab7a8aa784
Hristo Tanchev <h.tanchev@emerchantpay.com>
2014-11-03 20:57:50 +0200

    Added new tabel client and add FK to it

--- protected/data/database_schema.sql
+++ protected/data/database_schema.sql
@@ -2,10 +2,19 @@ CREATE DATABASE IF NOT EXISTS script_lang_first;
 
 USE script_lang_first;
 
-CREATE TABLE IF NOT EXISTS tbl_area (
-	id INT NOT NULL AUTO_INCREMENT,
-	area_name VARCHAR(25) NOT NULL,
-	geo GEOMETRY,
-	PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS tbl_client (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(25) NOT NULL,
+        info TEXT,
+        position GEOMETRY,
+        PRIMARY KEY(id)
 );
 
+CREATE TABLE IF NOT EXISTS tbl_area (
+        id INT NOT NULL AUTO_INCREMENT,
+        area_name VARCHAR(25) NOT NULL,
+        geo GEOMETRY,
+        client_id INT NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY (client_id) REFERENCES tbl_client(id)
+);
